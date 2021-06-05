8f8a3b458496fd91264031f252031aed5939ca41
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-06-05 17:42:41 +0100

    feat: added Home page title

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-
 const Home = ({props}) =>
    (
     <div>
-
+     <h1>Git Hub Language Finder</h1>
     </div>
     );
 
--- src/components/Home.test.js
+++ src/components/Home.test.js
@@ -4,3 +4,8 @@ import Home from './Home';
 test('renders Homepage', () => {
   render(<Home />);
 });
+
+test('check page title exists', () => {
+  const {getByText} = render(<Home/>)
+  expect(getByText("Git Hub Language Finder")).toBeInTheDocument();
+});
