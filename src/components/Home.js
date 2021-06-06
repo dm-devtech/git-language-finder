334fdbc820e4caa4f834611a8a683ab3f581dba7
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-06-06 15:23:49 +0100

    feat: form submission with alert

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Home = ({props}) =>
-   (
+function Home() {
+  const [user, setUser] = React.useState("");
+
+  const handleSubmit = (e) => {
+     e.preventDefault();
+     alert(`Submitting User: ${user}`)
+   }
+
+  return (
     <div>
      <h1>Git Hub Language Finder</h1>
 
-     <form onSubmit={props}>
+     <form onSubmit={handleSubmit}>
       <textarea
+        onChange={(e) => setUser(e.target.value)}
         data-testid='input-field'
         style={{width: "200px"}}
+        name='user'
+        value={user}
        />
       <button type='submit' data-testid='Submit'>Submit</button>
      </form>
     </div>
     );
+  }
 
 export default Home;
--- src/components/Home.test.js
+++ src/components/Home.test.js
@@ -1,4 +1,4 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import Home from './Home';
 
 test('renders Homepage', () => {
@@ -10,15 +10,55 @@ test('check page title exists', () => {
   expect(getByText("Git Hub Language Finder")).toBeInTheDocument();
 });
 
-test("submit button can be clicked", () => {
-  const onSubmit = jest.fn(e => e.preventDefault());
-  const { getByText } = render(<Home props={onSubmit} />);
-  const submitButton = getByText("Submit")
-  fireEvent.click(submitButton);
-  expect(onSubmit).toHaveBeenCalledTimes(1);
-});
+// test.only("submit button can be clicked", () => {
+//   const onSubmit = jest.fn(e => e.preventDefault());
+//   const { getByText } = render(<Home handleSubmit={onSubmit} />);
+//   const submitButton = getByText("Submit")
+//   fireEvent.click(submitButton);
+//   expect(onSubmit).toHaveBeenCalledTimes(1);
+// });
 
-test('check input exists', () => {
+test('check input field exists', () => {
   const {getByTestId, getByText} = render(<Home />)
   expect(getByTestId("input-field")).toBeInTheDocument();
 });
+
+test('When form submitted alert message appears', async () => {
+  const alertSpy = jest.spyOn(window, 'alert').mockImplementation(() => {});
+
+  const { getByTestId } = render(<Home />);
+  const submitButton = getByTestId("Submit")
+
+  fireEvent.change(getByTestId("input-field"), { target: { value: 'octocat' } });
+  fireEvent.click(submitButton);
+
+  expect(alertSpy).toHaveBeenCalledTimes(1);
+  expect(alertSpy).toHaveBeenCalledWith("Submitting User: octocat")
+})
+
+test('no form data given', async () => {
+  const alertSpy = jest.spyOn(window, 'alert').mockImplementation(() => {});
+
+  const { getByTestId } = render(<Home />);
+  const submitButton = getByTestId("Submit")
+
+  fireEvent.change(getByTestId("input-field"), { target: {  } });
+  fireEvent.click(submitButton);
+
+  expect(alertSpy).toHaveBeenCalledTimes(1);
+  expect(alertSpy).not.toHaveBeenCalledWith("Submitting User: octocat")
+})
+
+// test input gets put into fetch
+
+// test input then submit does something
+
+// EC - invalid user
+
+// EC - no repos
+
+// EC - most popular language = null
+
+// EC - tied top languages
+
+// css
